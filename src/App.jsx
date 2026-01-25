803333a01bfd11629fdc8e86906049b2239bb20d
hussainALJ <149183695+hussainALJ@users.noreply.github.com>
2026-01-25 22:56:48 +0300

    Add hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Header from './components/Header.jsx'
+import Hero from './components/Hero.jsx'
 
 function App() {
   return (
     <>
       <Header />
+      <Hero />
     </>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+function Hero () {
+    return (
+        <>
+          <section className="hero">
+            <div className="container">
+                <h1>Simple ToDo App</h1>
+                <p>This todo app for test design.</p>
+            </div>
+          </section>
+        </>
+    )
+}
+
+export default Hero
